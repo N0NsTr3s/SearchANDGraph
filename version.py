33e4e71997d62d21fd82016857522be2b2b2e13f
Ricu Eduard <ricueduard@gmail.com>
2025-12-16 21:47:11 +0200

    chaged version for testing purposes

--- version.py
+++ version.py
@@ -3,7 +3,7 @@
 This module is intentionally tiny so it can be imported very early at startup.
 """
 
-CURRENT_VERSION = "1.0.8"
+CURRENT_VERSION = "1.0.9"
 
 # GitHub repository used for update checks (GitHub Releases).
 REPO_OWNER = "N0NsTr3s"
